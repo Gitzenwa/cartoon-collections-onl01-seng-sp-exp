6c2659822dc166b404e1f717760e383c39e12235
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-27 03:22:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheeses|
-  if array.include?(cheese_types)
+   cheese_types.include?(cheeses)
 
 end
