a856f8a129b9f6cb92bf76b9c88be79b90e8e306
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-27 03:07:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.collect do |word|
-    word.capitalize + "!" 
+    word.capitalize + "!"
+  end 
 end
 
 def long_planeteer_calls# code an argument here
