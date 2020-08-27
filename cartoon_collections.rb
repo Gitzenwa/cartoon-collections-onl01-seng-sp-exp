57d2b7901678ec7aeab324dcb2682b25d6287350
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-27 02:52:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |dwarve, number|
-    puts "#{number}. #{dwarve}"
+    puts "#{number + 1}. #{dwarve}"
   end
 end
 
